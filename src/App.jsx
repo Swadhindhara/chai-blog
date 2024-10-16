865f3ca8eae1324942babc8c64c3d9a02c3a323d
SWADHIN DHARA <you@example.com>
2024-10-16 17:37:53 +0530

    swadhin

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
   }, [])
   
   return !loading ? (
-    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-200">
       <div className="block w-full">
         <Header />
         <main>
